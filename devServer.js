3690ebd49597595a3f02a105c5b2b0707980d89b
Patrick Pan <patrick.pan@patrickpan.com>
2017-11-07 19:24:22 -0500

    Handle server errors

--- devServer.js
+++ devServer.js
@@ -9,14 +9,58 @@ const webpackHotMiddleware = require('webpack-hot-middleware');
 const emptyCache = require('require-reload').emptyCache;
 
 // Initialize express app
-let app = express();
+const app = express();
 
 // Import webpack configuration
 const config = require('./webpack/webpack.config.js');
 const PORT = process.env.PORT || config._port;
 const compiler = webpack(config);
 
-// Add webpack middleware
+// Server starts initially unloaded
+let server = null;
+let serverLoadError = "Server is not yet loaded.";
+
+// Attempt to load the server, setting error appropriately if it fails
+const loadServer = () => {
+    try {
+        server = require('./server/entry');
+        serverLoadError = null;
+        console.log("Successfully loaded server!");
+    } catch (e) {
+        serverLoadError = e.toString();
+        console.error("Error loading server: " + serverLoadError);
+        server = null;
+    }
+}
+
+// Listen for change in server
+const serverWatch = chokidar.watch('./server/**/*.*');
+serverWatch.on('ready', () => {
+    // Load the server for the first time
+    loadServer();
+
+    // Reload the server on any change
+    serverWatch.on('all', () => {
+        console.log("Reloading server");
+        // Fully re-require server
+        if (server !== null) {
+            emptyCache(server.context);
+        }
+        loadServer();
+    })
+});
+
+// Handle server requests
+app.use((req, res, next) => {
+    // If there is an error, send the error on any request
+    if (serverLoadError !== null) {
+        return res.status(500).end("Server load error: " + serverLoadError);
+    }
+    
+    server(req, res, next)
+});
+
+// Add webpack middlewares
 app.use(webpackDevMiddleware(compiler, {
     stats: {
         colors: true,
@@ -24,31 +68,11 @@ app.use(webpackDevMiddleware(compiler, {
         chunks: false,
     }
 }));
-
 app.use(webpackHotMiddleware(compiler, {
-    log: console.log
+    log: console.log,
 }));
 
-// Import server as hot-reload
-let loadServer = () => require('./server/entry');
-let server = loadServer()
-
-// Wrap server in a function so that it can change
-app.use((req, res, next) => {
-    server(req, res, next)
-})
-
-// Listen for change in server
-const serverWatch = chokidar.watch('./server/**/*.*');
-serverWatch.on('ready', () => {
-    serverWatch.on('all', () => {
-        console.log("Reloading server");
-        // Re-require server
-        emptyCache(server.context);
-        server = loadServer();
-    })
-})
-
+// Set app to listen on specified port
 app.listen(PORT, function() {
     console.log("Now listening on port " + PORT + " :)");
 });
--- server/routes/helloWorld.js
+++ server/routes/helloWorld.js
@@ -5,6 +5,6 @@ let router = express.Router();
 
 router.get('/', (req, res) => {
     res.end(`hello ${helloWorldAPI.getString()}`);
-})
+});
 
 module.exports = router;
