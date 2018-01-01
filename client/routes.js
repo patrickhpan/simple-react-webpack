91a811116a1ed083d4e7f08b1de3c78d5a32cc14
Patrick Pan <patrick.pan@patrickpan.com>
2018-01-01 10:52:34 -0500

    Fixed routes file as well

--- client/routes.js
+++ client/routes.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import App from './containers/App';
-import HelloWorld from './containers/HelloWorld';
-import About from './containers/About';
+import App from 'containers/App';
+import HelloWorld from 'containers/HelloWorld';
+import About from 'containers/About';
 
 export default [
     <Route exact path="/" component={HelloWorld} />,
